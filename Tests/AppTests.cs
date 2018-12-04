3b412dc63a8a3884d10656f2add848947a49213b
Daniel Harrison <harrison92d+orangudan@gmail.com>
2018-12-04 22:08:27 +0000

    Share app route setup

--- Tests/AppTests.cs
+++ Tests/AppTests.cs
@@ -6,38 +6,42 @@ namespace Tests
 {
     public class AppTests
     {
+        private App _app;
+
+        [SetUp]
+        public void Set_up_routes()
+        {
+            _app = new App();
+            _app.AddRoute("home", "Content for home page");
+            _app.AddRoute("about", "Content for about page");
+            _app.AddRoute("contact", "Content for contact page");
+        }
+
         [Test]
         public void Home_returns_home_page_content()
         {
-            var app = new App();
-            app.AddRoute("home", "Content for home page");
-            var result = app.Handle("home");
+            var result = _app.Handle("home");
             Assert.That(result, Is.EqualTo("Content for home page"));
         }
 
         [Test]
         public void About_returns_about_page_content()
         {
-            var app = new App();
-            app.AddRoute("about", "Content for about page");
-            var result = app.Handle("about");
+            var result = _app.Handle("about");
             Assert.That(result, Is.EqualTo("Content for about page"));
         }
 
         [Test]
         public void Contact_returns_contact_page_content()
         {
-            var app = new App();
-            app.AddRoute("contact", "Content for contact page");
-            var result = app.Handle("contact");
+            var result = _app.Handle("contact");
             Assert.That(result, Is.EqualTo("Content for contact page"));
         }
 
         [Test]
         public void Unmatched_path_throws_argument_exception()
         {
-            var app = new App();
-            Assert.Throws<ArgumentException>(() => app.Handle("alskdj"));
+            Assert.Throws<ArgumentException>(() => _app.Handle("Unmatched path"));
         }
     }
 }
